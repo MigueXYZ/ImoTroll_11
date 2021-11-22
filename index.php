6c95f3ee2a1399e60e92039963d1c377aa2f11c9
MigueXYZ <Miguel553013>
2021-11-22 12:05:26 +0000

    alterações MigueXYZ

--- index.php
+++ index.php
@@ -43,14 +43,15 @@
         <div class="container px-4 px-lg-5">
             <!-- Heading Row-->
             <div class="row gx-4 gx-lg-5 align-items-center my-5">
-                <div class="col-lg-7"><img class="img-fluid rounded mb-4 mb-lg-0" src="imagens/thumbnail.jpg" /></div>
+                <div class="col-lg-7 "><img class="img-fluid rounded mb-4 mb-lg-0" src="imagens/thumbnail.jpg" /></div>
                 <div class="col-lg-5">
                     <h1 class="font-weight-light">ImovTrolls</h1>
                     <p> A pequena empresa com a caverna ideal para si!</p>
                 </div>
             </div>
+
             <!-- Content Row-->
-            <div class="row gx-4 gx-lg-5" id="preenche">
+            <div class="row gx-4 gx-lg-5 mt-3" id="preenche">
 
             </div>
         </div>
--- listar.php
+++ listar.php
@@ -9,20 +9,38 @@ switch ($ordem){
 };
 $sql="select * from Imoveis ".$ordem;
 $result=mysqli_query($con,$sql);
+?>
+    <div class="row gx-4 gx-lg-5 mb-3">
+        <div class="dropdown">
+            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Ordena
+            </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
+                <button class="dropdown-item" type="button">Mais Barato</button>
+                <button class="dropdown-item" type="button">Mais Caro</button>
+                <button class="dropdown-item" type="button">Mais Recente</button>
+            </div>
+        </div>
+    </div>
+<?php
 
 
 while($dados=mysqli_fetch_array($result)){
     $sqlIMG="select * from imagens where imagemImovelId= ".$dados['imovelId']." and imagemTipo='principal'";
     $resultIMG=mysqli_query($con,$sqlIMG);
     $img=mysqli_fetch_array($resultIMG);
+
     ?>
+
 <div class="col-md-4 mb-5">
     <div class="card h-100">
-        <img class="card-img-top" src="<?php echo $img['imagemCaminhoURL'] ?>">
+        <img class="card-img-top" style="height:177px;" src="<?php echo $img['imagemCaminhoURL'] ?>">
         <div class="card-body">
             <h2 class="card-title"><?php echo $dados['imovelNome']?></h2>
         </div>
-        <div class="card-footer"><a class="btn btn-primary btn-sm" href="#!">More Info</a></div>
+        <div class="card-footer  my-auto">
+            <a class="btn btn-primary btn-sm" href="#!">More Info</a><span class="mt-2" style="float:right;">Preço: <?php echo $dados['imovelPreco']?> €</span>
+        </div>
     </div>
 </div>
 <?php
